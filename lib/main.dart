55c408030e88b9dc6ae6126299fe87e005fd72bd
Saddam Afzal <saddamafzal07@gmail.com>
2021-06-22 03:30:18 +0500

    Work accomplish

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'I am Rich ',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Homepage(),
+    );
+  }
+}
+
+class Homepage extends StatelessWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("I am rich"),
+      ),
+      body: Container(),
+    );
+  }
+}
